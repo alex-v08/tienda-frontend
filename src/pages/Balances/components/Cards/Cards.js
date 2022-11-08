b953e00ad468f0b72357fdd59908117c2040db83
Leonardo Duce <LDUCE@PROVEEDOR.TECO.COM.AR>
2022-11-08 15:09:13 -0300

    Pagina balances mock.

---
+++ src/pages/Balances/components/Cards/Cards.js
@@ -0,0 +1,17 @@
+import './styles/cards.css';
+
+export const Cards = (balances) => {
+    
+    return ` <div class="balance-card">                  
+                <h2>1 cliente</h2>
+            </div>                    
+            <div class="balance-card">                  
+                <h2>${balances.ventas.length} ventas</h2>
+            </div>   
+            <div class="balance-card">                  
+                <h2>${20} juegos</h2>
+            </div> 
+            <div class="balance-card">                  
+                <h2>$ ${balances.recaudacion}</h2>
+            </div>`;     
+} 
---
+++ src/pages/Balances/components/Venta/Venta.js
@@ -0,0 +1,29 @@
+import "./styles/venta.css";
+
+export const Venta = (venta) => {
+  return `<div class="venta"> 
+            <div class="info">                 
+                <h3>Fecha: ${venta.fecha} </h2>
+            </div>
+            <div class="productos">  
+                <div class="header">                   
+                    <div>Juego</div>
+                    <div>Precio</div>
+                    <div>Cantidad</div>
+                </div>
+                ${venta.productos.map(
+                  (producto) => `<div class="producto" key="${producto.productoId}">                   
+                                    <div>${producto.nombre}</div>
+                                    <div>${producto.precio}</div>
+                                    <div>${producto.cantidad}</div>
+                                </div>`
+                ).join("")}
+                <div class="footer">     
+                    <div></div>                 
+                    <div>TOTAL</div>                               
+                    <div>10000</div>
+                </div>
+               
+            </div>
+        </div> `;
+};
