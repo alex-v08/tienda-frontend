b8e91fae5e0a6c91b43080bc5e813ba1e7011ca1
Leonardo Duce <LDUCE@PROVEEDOR.TECO.COM.AR>
2022-11-08 16:12:53 -0300

    Add active link logic

--- src/init.js
+++ src/init.js
@@ -1,1 +1,19 @@
-import 'victormono'
+import "victormono";
+
+const activateLink = () => {
+  const activePage = window.location.pathname;
+  const navLinks = document.querySelectorAll("nav a");
+
+  if (activePage !== "/") {
+    navLinks.forEach((link) => {
+      if (link.href.includes(`${activePage}`)) {
+        link.classList.add("active");
+      }
+    });
+  }
+  if (activePage === "/") 
+    navLinks[0].classList.add("active");
+
+};
+
+window.addEventListener("DOMContentLoaded", activateLink);
